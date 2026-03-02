f5fb87094403c1eca9d89e9a61692cfac972b055
Muhammed Uzair <nayazmuhammed094@gmail.com>
2026-03-02 16:45:04 +0400

    feat(e4): complete e4

--- ch2/e4/main.go
+++ ch2/e4/main.go
@@ -1,5 +1,15 @@
 package main
 
-func marshalAll[T any](items []T) ([][]byte, error) {
+import "encoding/json"
 
+func marshalAll[T any](items []T) ([][]byte, error) {
+	var result [][]byte
+	for _, v := range items {
+		marshaledData, err := json.Marshal(v)
+		if err != nil {
+			return [][]byte{}, err
+		}
+		result = append(result, marshaledData)
+	}
+	return result, nil
 }
