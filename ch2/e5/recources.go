521dc3e0d12a8fa5493c749f07f195ee6f07dbfa
Muhammed Uzair <nayazmuhammed094@gmail.com>
2026-03-02 17:14:52 +0400

    feat(e5): complete e5

--- ch2/e5/recources.go
+++ ch2/e5/recources.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"sort"
@@ -16,14 +17,22 @@ func getResources(url string) ([]map[string]any, error) {
 
 	defer res.Body.Close()
 
-	// ?
+	dec := json.NewDecoder(res.Body)
+	if err := dec.Decode(&resources); err != nil {
+		return nil, err
+	}
 
+	return resources, nil
 }
 
 func logResources(resources []map[string]any) {
 	var formattedStrings []string
 
-	// ?
+	for _, item := range resources {
+		for key, value := range item {
+			formattedStrings = append(formattedStrings, fmt.Sprintf("Key: %s - Value: %v", key, value))
+		}
+	}
 
 	sort.Strings(formattedStrings)
 
