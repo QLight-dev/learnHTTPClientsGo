5e023f3660d5d3712d3b82d56cd6b2dc5c963209
Muhammed Uzair <nayazmuhammed094@gmail.com>
2026-03-03 14:54:05 +0400

    feat(e1): complete e1

--- ch4/e1/main.go
+++ ch4/e1/main.go
@@ -10,14 +10,19 @@ func newParsedURL(urlString string) ParsedURL {
 		return ParsedURL{}
 	}
 
+	var parsedURLPassword string
+	// make sure we don't  return a User without a Password
+	if password, isSet := parsedUrl.User.Password(); isSet {
+		parsedURLPassword = password
+	}
 	return ParsedURL{
-		protocol: "",
-		username: "",
-		password: "",
-		hostname: "",
-		port:     "",
-		pathname: "",
-		search:   "",
-		hash:     "",
+		protocol: parsedUrl.Scheme,
+		username: parsedUrl.User.Username(),
+		password: parsedURLPassword,
+		hostname: parsedUrl.Hostname(),
+		port:     parsedUrl.Port(),
+		pathname: parsedUrl.Path,
+		search:   parsedUrl.RawQuery,
+		hash:     parsedUrl.Fragment,
 	}
 }
