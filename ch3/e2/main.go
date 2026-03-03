3790c38bff5e88c166fbe9c75c6c4fd71ac9388f
Muhammed Uzair <nayazmuhammed094@gmail.com>
2026-03-03 14:23:52 +0400

    feat(e2): complete e2

--- ch3/e2/main.go
+++ ch3/e2/main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"fmt"
+	"net/url"
+)
+
 func getDomainNameFromURL(rawURL string) (string, error) {
-	
+	parsedURL, err := url.Parse(rawURL)
+	if err != nil {
+		return "", fmt.Errorf("Error when parsing URL: %q", err)
+	}
+	return parsedURL.Hostname(), nil
 }
