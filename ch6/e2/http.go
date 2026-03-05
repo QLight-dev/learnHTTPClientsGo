aa27debb2264cbbb82510ca911b9bf5f3292429b
Muhammed Uzair <nayazmuhammed094@gmail.com>
2026-03-05 17:01:57 +0400

    feat(e2): complete e2

--- ch6/e2/http.go
+++ ch6/e2/http.go
@@ -7,5 +7,27 @@ import (
 )
 
 func createUser(url, apiKey string, data User) (User, error) {
-	// ?
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		return User{}, err
+	}
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonData))
+
+	req.Header.Set("Content-Type", "application/jsonData")
+	req.Header.Set("X-API-Key", apiKey)
+
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		return User{}, err
+	}
+
+	var user User
+	dec := json.NewDecoder(res.Body)
+	if err := dec.Decode(&user); err != nil {
+		return User{}, err
+	}
+
+	return user, nil
 }
