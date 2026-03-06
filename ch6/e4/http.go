d862ba6f06da64ba957c3d07dafe5f09aee9fc4f
Muhammed Uzair <nayazmuhammed094@gmail.com>
2026-03-06 21:24:03 +0400

    feat(e4): complete e4

--- ch6/e4/http.go
+++ ch6/e4/http.go
@@ -1,13 +1,61 @@
 package main
 
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+)
+
 func updateUser(baseURL, id, apiKey string, data User) (User, error) {
 	fullURL := baseURL + "/" + id
 
-	// ?
+	encodedData, err := json.Marshal(data)
+	if err != nil {
+		return User{}, err
+	}
+
+	req, err := http.NewRequest("PUT", fullURL, bytes.NewBuffer(encodedData))
+
+	if err != nil {
+		return User{}, err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("X-API-Key", apiKey)
+
+	client := &http.Client{}
+	res, err := client.Do(req)
+
+	var result User
+	decoder := json.NewDecoder(res.Body)
+	if err := decoder.Decode(&result); err != nil {
+		return User{}, err
+	}
+
+	return result, nil
 }
 
 func getUserById(baseURL, id, apiKey string) (User, error) {
 	fullURL := baseURL + "/" + id
 
-	// ?
+	req, err := http.NewRequest("GET", fullURL, nil)
+	if err != nil {
+		return User{}, err
+	}
+
+	req.Header.Set("X-API-Key", apiKey)
+
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		return User{}, err
+	}
+
+	var result User
+	dec := json.NewDecoder(res.Body)
+	if err := dec.Decode(&result); err != nil {
+		return User{}, err
+	}
+	
+	return result, nil
 }
