6d786bce819aa8f0b4a1fe715b1ee1d16e6b2699
Muhammed Uzair <nayazmuhammed094@gmail.com>
2026-03-06 21:43:21 +0400

    feat(e5): complete e5

--- ch6/e5/http.go
+++ ch6/e5/http.go
@@ -1,7 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 func deleteUser(baseURL, id, apiKey string) error {
 	fullURL := baseURL + "/" + id
 
-	// ?
+	req, err := http.NewRequest("DELETE", fullURL, nil)
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("X-API-Key", apiKey)
+
+	client := http.DefaultClient
+	res, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	if res.StatusCode < 200 || res.StatusCode >= 300 {
+		return fmt.Errorf("received non-succesful status code %v", res.StatusCode)
+	}
+	return nil
 }
