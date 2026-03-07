7cd105b476ed82a2a5729848bdd7ec6461cd5dfc
Muhammed Uzair <nayazmuhammed094@gmail.com>
2026-03-07 14:42:05 +0400

    feat(e3): complete e3

--- ch7/e3/main.go
+++ ch7/e3/main.go
@@ -1,8 +1,19 @@
 package main
 
-func fetchTasks(baseURL, availability string) []Issue {
-	// ?
+import "fmt"
 
-	fullURL := baseURL
+func fetchTasks(baseURL, availability string) []Issue {
+	availabilityIssues := 0
+	// check availablity to add to limit query parameter
+	switch availability {
+	case "Low":
+		availabilityIssues = 1
+	case "Medium":
+		availabilityIssues = 3
+	case "High":
+		availabilityIssues = 5
+	}
+
+	fullURL := fmt.Sprintf("%s?sort=estimate&limit=%v", baseURL, availabilityIssues)
 	return getIssues(fullURL)
 }
